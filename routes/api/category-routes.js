de767202a550ae62e64c44d638d4664b90064fdc
Spencer Kerans <skerans0@gmail.com>
2022-03-29 10:47:35 -0600

    added put and delete to category-route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -25,7 +25,7 @@ router.get('/:id', async (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json({ message: `That category doesn't exist`});
+      res.status(404).json({ message: `That category doesn't exist.`});
       return;
     }
     res.status(200).json(categoryData);
@@ -47,10 +47,14 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try{
-    const categoryData = await Category.update(req.body, {
-      where: { id: req.params.id }
+    const categoryData = await Category.update(
+      { category_name: req.body.category_name }, 
+      { where: { id: req.params.id },
     });
-
+    if(!categoryData) {
+      res.status(404).json({ message: `That category doesn't exist.`})
+    }
+    res.status(200).json(categoryData);
   } catch(err) {
     res.status(500).json(err);
   }
@@ -59,7 +63,13 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
   try{
-
+    const categoryData = await Category.destroy( 
+      { where: { id: req.params.id },
+    });
+    if(!categoryData) {
+      res.status(404).json({ message: `That category doesn't exist.`})
+    }
+    res.status(200).json(categoryData);
   } catch(err) {
     res.status(500).json(err);
   }
