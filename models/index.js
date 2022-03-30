57c1d55865913a0f61d8dd364a6a71764a0408bd
Spencer Kerans <skerans0@gmail.com>
2022-03-30 11:39:22 -0600

    added sequalize, routes for product/tags, cascade delete

--- models/index.js
+++ models/index.js
@@ -7,6 +7,7 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 // Categories have many Products
 Category.hasMany(Product, {
@@ -16,19 +17,11 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-},
-  as: 'tags',
-  foreignKey: 'product_id'
+  through: { model: ProductTag }
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-},
-  as: 'products',
-  foreignKey: 'tag_id'
+  through: { model: ProductTag }
 });
 
 module.exports = {
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -118,6 +118,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try{
+    const productData = await Product.destroy( 
+      { where: { id: req.params.id },
+    });
+    if(!productData) {
+      res.status(404).json({ message: `That product doesn't exist.`})
+    }
+    res.status(200).json(productData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
